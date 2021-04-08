9195f893aeb8f8662af59c0e69aa6681b0553dc0
Erick Vieira <fs.erickvieira@gmail.com>
2021-04-07 22:59:34 -0300

    Configurações de lint e sequelize

---
+++ API/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+    'prettier',
+  ],
+  puglins: ['prettier'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'class-methods-use-this': 'off',
+    'no-param-reassign': 'off',
+    camelcase: 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
+  },
+};
